7077f3587dccd7b6ed3665eff7b8d778f278b299
AarnoLempinen <aarno.antti@hotmail.com>
2024-02-02 17:03:44 +0200

    flatlist

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { FlatList, SafeAreaView, ScrollView, StyleSheet, Text, View, Image } from 'react-native';
+import Constants from 'expo-constants';
+import {DATA} from './Data';
+import Row from './components/Row'
+import Search from './components/Search';
 
 export default function App() {
+
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    setItems(DATA);
+  }, [])
+
+  const renderItem = ({item}) => (
+    <Text key={item.id}>
+      {item.firstname} {item.lastname}
+       </Text>
+  );
+
+  const executeSearch = (search) => {
+    const searchArray = DATA.filter((item) => item.lastname.startsWith(search));
+    setItems(searchArray);
+  };
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Search executeSearch={executeSearch} />
+      <FlatList
+      data={items}
+      renderItem={({item}) => (
+        <Row person = {item} />
+      )}
+      ></FlatList>
+    </SafeAreaView>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
+    paddingTop: Constants.statusBarHeight,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    paddingLeft: 8,
+    paddingRight: 8,
     justifyContent: 'center',
   },
+  rowContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    marginTop: 5,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderBottomLeftRadius: 5,
+    borderTopLeftRadius: 5
+  },
+  image: {
+    marginRight: 16
+  },
+  rowText: {
+    fontSize: 16,
+    marginLeft: 5,
+    padding: 1,
+  }
 });
---
+++ Data.js
@@ -0,0 +1,81 @@
+
+
+export const DATA = [
+    {
+        id: '1',
+        firstname: 'Jaakko',
+        lastname: 'Juntunen'
+    },
+    {
+        id: '2',
+        firstname: 'Henri',
+        lastname: 'Virtanen'
+    },
+    {
+        id: '3',
+        firstname: 'Juuso',
+        lastname: 'Jaakola'
+    },
+    {
+        id: '4',
+        firstname: 'Matti',
+        lastname: 'Heikkilä'
+    },
+    {
+        id: '5',
+        firstname: 'Markus',
+        lastname: 'Penttinen'
+    },
+    {
+        id: '6',
+        firstname: 'Jouku',
+        lastname: 'Pekkala'
+    },
+    {
+        id: '7',
+        firstname: 'Panu',
+        lastname: 'Perttunen'
+    },
+    {
+        id: '8',
+        firstname: 'Petteri',
+        lastname: 'Savo'
+    },
+    {
+        id: '9',
+        firstname: 'Simo',
+        lastname: 'Kinnunen'
+    },
+    {
+        id: '10',
+        firstname: 'Martta',
+        lastname: 'Korhonen'
+    },
+    {
+        id: '11',
+        firstname: 'Anneli',
+        lastname: 'Karhula'
+    },
+    {
+        id: '12',
+        firstname: 'Vesa',
+        lastname: 'Rasila'
+    },
+    {
+        id: '13',
+        firstname: 'Kaisa',
+        lastname: 'Hirviniemi'
+    },
+    {
+        id: '14',
+        firstname: 'Jaana',
+        lastname: 'Liukkonen'
+    },
+    {
+        id: '15',
+        firstname: 'Teppo',
+        lastname: 'Kivi'
+    },
+
+
+]
